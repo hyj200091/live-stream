a8be475e0700365b01dfe606c729145f575d736b
侯粤嘉 <2298090131@qq.com>
2020-10-28 16:08:01 +0800

    底部导航栏 首页布局 列表组件封装

---
+++ live-stream/components/common/l-list.vue
@@ -0,0 +1,45 @@
+<template>
+	<!-- 列表组件封装 -->
+	<!-- <view class="flex flex-wrap"> -->
+		<view class="list-item">
+			<image :src="item.image" style="width: 365rpx; height: 365rpx;" class="rounded" mode="aspectFill">
+			</image>
+			
+			<view class="rounded-circle px-2 flex align-center" style="position: absolute;left: 15rpx;top: 15rpx;background-color: rgba(0,0,0,0.4);">
+				<text class="iconfont iconbizhongguanli text-warning mr-1"></text><text class="text-white font">{{item.rank}}</text>
+			</view>
+			
+			<view class="rounded-circle px-2 flex align-center" style="position: absolute;right: 15rpx;top: 15rpx;background-color: rgba(0,0,0,0.4);">
+				<text class="font-sm text-white">人气:</text><text class="text-white font-sm">{{item.people}}</text>
+			</view>
+			
+			<view class="rounded-circle flex align-center" style="position: absolute;left: 15rpx;bottom: 15rpx;">
+				<text class="text-white font">{{item.title}}</text>
+			</view>
+			
+			<view class="rounded-circle px-2 flex align-center" style="position: absolute;right: 15rpx; bottom: 15rpx; background-color: rgba(0,0,0,0.4);" >
+				<text style="width: 20rpx; height: 20rpx;" class="rounded-circle bg-danger mr-1">
+				</text><text class="text-white font-sm">已结束</text>
+			</view>
+		</view>
+	<!-- </view> -->
+</template>
+
+<script>
+	export default {
+		props:{
+			item: Object,
+			index: [Number,String]
+		}
+	}
+</script>
+
+<style>
+	.list-item{
+		width: 375rpx;
+		height: 375rpx;
+		padding: 5rpx;
+		box-sizing: border-box;
+		position: relative;
+	}
+</style>
--- live-stream/pages/index/index.vue
+++ live-stream/pages/index/index.vue
@@ -9,14 +9,79 @@
 					<text class="iconfont iconmore mr-2 text-white"></text>
 				</template>
 		</nav-bar>
+		<view style="margin-top: 24px;">
+			<!-- 轮播图 -->
+			<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="200" style="width: 750rpx;height: 250rpx;">
+				<swiper-item>
+					<image src="../../static/beijing2.jpg" style="width: 750rpx;height: 250rpx;"></image>
+				</swiper-item>
+				<swiper-item>
+					<image src="../../static/beijing1.jpg" style="width: 750rpx;height: 250rpx;"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+		<view class="flex flex-wrap">
+			<l-list v-for="(item,index) in list" :key="index" :item="item" :index="index"></l-list>
+		</view>
+			
 	</view>
 </template>
 
 <script>
 import navBar from '../../components/common/nav-bar.vue';
+import lList from '../../components/common/l-list.vue'
 	export default {
 		components:{
-			navBar
+			navBar,lList
+		},
+		data() {
+			
+			return{
+				list:[
+				{
+					rank: 4,
+					image: 'https://soft1851.oss-cn-beijing.aliyuncs.com/markdown/2033810.jpg',
+					people: 990,
+					title: '二次元',
+					finish:0    //0表示结束 1 表示正在直播
+				},
+				{
+					rank: 1,
+					image: 'https://soft1851.oss-cn-beijing.aliyuncs.com/markdown/d81a17f4f7eb0d8a786b6ae9b4a2596.jpg',
+					people: 122,
+					title: '户外',
+					finish:0    //0表示结束 1 表示正在直播
+				},
+				{
+					rank: 3,
+					image: 'https://soft1851.oss-cn-beijing.aliyuncs.com/markdown/5b2887d00a6c35f5e882ac8295cb77a.jpg',
+					people: 324,
+					title: '健身',
+					finish: 0    //0表示结束 1 表示正在直播
+				},
+				{
+					rank: 6,
+					image: 'https://soft1851.oss-cn-beijing.aliyuncs.com/markdown/d81a17f4f7eb0d8a786b6ae9b4a2596.jpg',
+					people: 684,
+					title: '美食',
+					finish: 1    //0表示结束 1 表示正在直播
+				},
+				{
+					rank: 5,
+					image: 'https://soft1851.oss-cn-beijing.aliyuncs.com/markdown/5b2887d00a6c35f5e882ac8295cb77a.jpg',
+					people: 543,
+					title: '帅哥',
+					finish: 1    //0表示结束 1 表示正在直播
+				},
+				{
+					rank: 2,
+					image: 'https://soft1851.oss-cn-beijing.aliyuncs.com/markdown/2033810.jpg',
+					people: 1100,
+					title: '美女',
+					finish: 1    //0表示结束 1 表示正在直播
+				},
+				]
+			}
 		},
 		onLoad() {
 			// uni.hideTabBar();
@@ -25,9 +90,4 @@ import navBar from '../../components/common/nav-bar.vue';
 </script>
 
 <style>
-	.content {
-		width: 100%;
-		height: 100%;
-		background: #5a5b5a;
-	}
 </style>
